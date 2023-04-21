a31bf238f8bfec32026efb99b04b164542ac6cc7
mariam.01 <mariam.larhlid01@gmail.com>
2023-04-21 09:27:01 -0700

    var

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+char A[26] = "abcdefghijklmnopqrstuvwxyz";
+char B[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+for (i = 0; i < 26; i++)
+{
+putchar(A[i]);
+}
+for (i = 0; i < 26; i++)
+{
+putchar(B[i]);
+}
+putchar('\n');
+return (0);
+}
