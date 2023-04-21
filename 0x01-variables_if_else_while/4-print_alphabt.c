87b05edae7954cd776535e109f37239022584755
mariam.01 <mariam.larhlid01@gmail.com>
2023-04-21 10:08:09 -0700

    var

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+char A[26] = "abcdefghijklmnopqrstuvwxyz";
+for (i = 0; i < 26; i++)
+{
+if (A[i] != 'e' && A[i] != 'q')
+{
+putchar(A[i]);
+}
+}
+putchar('\n');
+return (0);
+}
