b524a8e109c698c516f9dae1d8622a93b3589e92
mariam.01 <mariam.larhlid01@gmail.com>
2023-04-21 19:44:58 -0700

    var

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Inventing is a combination of brains and materials
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i, j;
+for (i = 48; i <= 58; i++)
+{
+putchar(i);
+for (j = 49; j <= 57; j++)
+{
+if (j > i)
+{
+putchar(i);
+putchar(j);
+if (i != 56 || j != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
