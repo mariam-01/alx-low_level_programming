be0e9ac19eb1cd09fe32905aa7f81f4d1089773b
mariam.01 <mariam.larhlid01@gmail.com>
2023-04-25 14:53:43 -0700

    loop

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 /**
 * print_alphabet - print alphabet
 * Return: Always 0 (Success)
 */
 void print_alphabet(void)
 {
-char A[26] = "abcdefghijklmnopqrstuvwxyz"
+int i ;
+char A[26] = "abcdefghijklmnopqrstuvwxyz";
 for (int i = 0; i < 26; i++)
 {
 _putchar(A[i]);
