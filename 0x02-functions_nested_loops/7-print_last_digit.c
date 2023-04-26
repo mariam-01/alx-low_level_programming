872be5348d9bab61781e5790a4ab6bd1625682fa
mariam.01 <mariam.larhlid01@gmail.com>
2023-04-25 17:39:02 -0700

    loop

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,12 @@
  */
 int print_last_digit(int n)
 {
-if (n >= 0)
-{
-return (n % 10);
-}
-else
-{
-return (-n % 10);
-}
+int a;
+	if (n < 0)
+	n = -n;
+	a = n % 10;
+	if (a < 0)
+		a = -a;
+	_putchar(a + '0');
+	return (a);
 }
