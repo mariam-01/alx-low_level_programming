9597522947b27f0585d3214b3117c426318ada50
mariam.01 <mariam.larhlid01@gmail.com>
2023-04-26 19:06:14 -0700

    loop

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,7 +5,9 @@
  * @n: number of the times table
  */
 void print_times_table(int n)
+{
 	int i, j, k;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
@@ -43,3 +45,4 @@ void print_times_table(int n)
 		}
 	}
 }
+
