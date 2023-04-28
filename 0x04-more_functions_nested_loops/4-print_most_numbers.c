ac9b23d701773ea645101ad61fd579f727aebbef
mariam.01 <mariam.larhlid01@gmail.com>
2023-04-27 17:25:12 -0700

    f

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,2 +1,16 @@
 #include "main.h"
-
+/**
+* print_most_numbers - print most numbers
+* Return: Always 0 (Success)
+*/
+void print_most_numbers(void)
+{
+for (i = 48; i < 57; i++)
+{
+if ((i != 50) || (i != 52))
+{
+_putchar(i);
+}
+}
+_putchar('\n');
+}
