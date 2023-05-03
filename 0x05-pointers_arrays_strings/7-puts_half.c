c378d2b42f069df20291af31523781e15ddadb81
mariam.01 <mariam.larhlid01@gmail.com>
2023-05-02 19:04:00 -0700

    p

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,9 +7,17 @@
  */
 void puts_half(char *str)
 {
-int l, i;
+int l, i, n;
 l = strlen(str);
-for (i = l / 2; i < l; i++)
+if (l % 2 == 0)
+{
+n = l / 2;
+}
+else
+{
+n = (l / 2) + 1;
+}
+for (i = n; i < l; i++)
 {
 _putchar(str[i]);
 }
