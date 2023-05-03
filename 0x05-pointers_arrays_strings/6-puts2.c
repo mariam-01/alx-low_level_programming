d90827f2e93557d9d9f244af2c344d7089c041ca
mariam.01 <mariam.larhlid01@gmail.com>
2023-05-02 18:10:02 -0700

    p

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * puts2- prints every other character of a string
+ * @str: string
+ */
+void puts2(char *str);
+{
+int l, i;
+for (i = 0; i < l; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
