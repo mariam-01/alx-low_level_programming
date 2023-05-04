7e2efad43fe4833847a527ec099c17f0ac4146a9
mariam.01 <mariam.larhlid01@gmail.com>
2023-05-04 16:19:15 -0700

    m

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * string_toupper - changes all lowercase to uppercase
+ *
+ *@s: pointer
+ * Return: 0
+ */
+char *string_toupper(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] >= 97 && s[i] <= 122)
+{
+    s[i] = s[i] - 32;
+}
+}
+return (s);
+}
