5e314fd66962f23f1df9333a35ee16ccce62b72e
mariam.01 <mariam.larhlid01@gmail.com>
2023-05-04 18:12:29 -0700

    m

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_number - print number
+ *
+ *@n: number
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+unsigned int m;
+if (n < 0)
+{
+_putchar('-');
+m = -n;
+}
+if (m / 10 != 0)
+{
+print_number(m / 10);
+}
+_putchar((m % 10) + '0');
+}
+return (str);
+}
+
