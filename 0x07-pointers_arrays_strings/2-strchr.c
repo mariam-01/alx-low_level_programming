2ac78aa9324745788081d9f6865975ed2b28c3ed
mariam.01 <mariam.larhlid01@gmail.com>
2023-05-08 12:20:44 -0700

    a

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,13 +12,13 @@
  */
 char *_strchr(char *s, char c)
 {
-while (*s != '\0')
+	unsigned int i;
+for (i = 0; s[i] >= '\0'; i++)
 {
-if (*s == c)
+if (s[i] == c)
 {
-return (s);
+return (s + i);
 }
-s++;
 }
-return (s);
+return ('\0');
 }
