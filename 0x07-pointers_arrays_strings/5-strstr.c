1ce2043a818f14c3f4ba6a1358d268f95f083488
mariam.01 <mariam.larhlid01@gmail.com>
2023-05-08 18:42:33 -0700

    a

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strstr -  search a string for any of a set of bytes.
+ *@haystack: pointer
+ *@needle: pointer
+ *
+ *
+ * Return: a pointer or null
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+char *hay, *nee;
+int i;
+if (*needle == '\0')
+{
+return (haystack);
+}
+while (*haystack != '\0')
+{
+hay = haystack;
+nee = needle;
+while (*nee != '\0' && *hay == *nee)
+{
+hay++;
+nee++;
+}
+if (*nee == '\0')
+{
+return (haystack);
+}
+haystack++;
+}
+return (0);
+}
