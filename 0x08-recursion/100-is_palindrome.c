d489ef323d6479a2ecee145f9358b6c5d56c42e6
mariam.01 <mariam.larhlid01@gmail.com>
2023-05-10 19:24:56 -0700

    r

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _palindrome - check for palandrome
+ *@s: pointer
+ *@debut: number
+ *@fin: number
+ * Return: number
+ */
+int _palindrome(char *s, int debut, int fin)
+{
+if (debut >= fin)
+{
+return (1);
+}
+else if (s[debut] != s[fin])
+{
+return (0);
+}
+else
+{
+return (i_palindrome(s, debut + 1, fin - 1));
+}
+}
+
+/**
+ * is_palindrome - returns 1 if n is a palindrome, otherwise return 0.
+ *@s: pointer
+ * Return: returns 1 if n is a palindrome, otherwise return 0
+ */
+
+int is_palindrome(char *s)
+{
+int l = strlen(s);
+return (_palindrome(s, 0, l - 1));
+}
+
