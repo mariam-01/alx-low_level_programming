98ec53e3eebb68adf08cb9c63be1643750071873
mariam.01 <mariam.larhlid01@gmail.com>
2023-05-12 12:21:51 -0700

    arg

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,12 +7,10 @@
  *
  * Return: returns 0
  */
-
-
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-
-printf("%d\n", argc - 1);
+int n = argc - 1;
+printf("%d\n", n);
 
 return (0);
 }
