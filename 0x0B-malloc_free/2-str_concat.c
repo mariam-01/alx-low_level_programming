02ad3b9245a6fe37eae8f816a246e151b0479116
mariam.01 <mariam.larhlid01@gmail.com>
2023-05-16 13:21:11 -0700

    mal

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,3 +3,41 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
+/**
+ * str_concat -  returns a pointer to a new string
+ *@s1: pointer
+ *@s2: pointer
+ *
+ * Return: returns a pointer, or NULL if it fails
+ */
+
+
+char *str_concat(char *s1, char *s2)
+{
+unsigned int i = 0, j = 0, l;
+char *a;
+if (s1 == NULL)
+{
+s1 == "";
+}
+else if (s2 == NULL)
+{
+s2 == "";
+}
+l = 1 + strlen(s1) + strlen(s2);
+a = malloc(l *sizeof(char));
+
+
+while (i < strlen(s1))
+{
+a[i] = s1[i];
+i++;
+}
+while (j < strlen(s2))
+{
+a[i + j] = s2[j];
+j++;
+}
+a[l] = '\0';
+return (a);
+}
