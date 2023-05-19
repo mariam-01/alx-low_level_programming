8db4b900448ea42b9b459af09cc7a6ebbcee12ac
mariam.01 <mariam.larhlid01@gmail.com>
2023-05-18 18:19:07 -0700

    aloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,4 +3,26 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
+/**
+ * _calloc -  llocates memory for an array, using malloc.
+ *
+ *@nmemb: integer
+ *@size: integer
+ * Return: pointer ,If it fails return NULL
+ */
 
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *a;
+if (size == 0 || nmemb == 0)
+{
+return (NULL);
+}
+a = malloc(nmemb * sizeof(char));
+if (a == 0)
+{
+return (NULL);
+}
+memset(ptr, 0, nmemb * size);
+return (a);
+}
