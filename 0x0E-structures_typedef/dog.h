97efab5327873fd5c690f18bfff03889be08f284
mariam.01 <mariam.larhlid01@gmail.com>
2023-05-22 18:51:11 -0700

    s

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+
+#ifndef dog_h
+#define dog_h
+
+
+/**
+ *struct dog - a structure names dog
+ *@name : pointer
+ *@owner : pointer
+ *@age : number
+ * Return: 0
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
