e2b5ed05b28dbb2ec9173b21db57e66fa07a5079
mariam-01 <mariam.larhlid2001@gmail.com>
2023-06-01 13:35:18 -0700

    list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+/**
+ * list_len -  return the number of all the elements of a list_t list.
+ *@h : struct
+ * Return: the number of element
+ */
+
+
