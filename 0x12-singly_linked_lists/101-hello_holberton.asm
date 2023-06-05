43e1de2978696753999b43eaa7eb0004b7cf35ed
mariam-01 <mariam.larhlid2001@gmail.com>
2023-06-04 18:23:12 -0700

    list

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,2 +1,23 @@
+section .data
+    format db "Hello, Holberton", 0
+    newline db 10, 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    sub rsp, 8                 ; Align the stack to 16 bytes
+    mov edi, format            ; Load format string address
+    xor eax, eax               ; Clear upper 32 bits of rax
+    call printf                ; Call printf
+
+    mov edi, newline           ; Load newline string address
+    xor eax, eax               ; Clear upper 32 bits of rax
+    call printf                ; Call printf
+
+    xor eax, eax               ; Return 0 from main
+    add rsp, 8                 ; Restore the stack
+    ret
 
 
