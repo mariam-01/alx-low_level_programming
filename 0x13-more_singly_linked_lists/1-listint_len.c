2c5635b664bfb5ebd34041e114284951f7e107a2
mariam-01 <mariam.larhlid2001@gmail.com>
2023-06-06 15:16:21 -0700

    list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,28 @@
+//#include "lists.h"
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+
+/**
+ * listint_len -  return the number of all the elements of a list_t list.
+ *@h : struct
+ * Return: the number of element
+ */
+
+size_t listint_len(const listint_t *h)
+{
+size_t cpt = 0;
+while (h != NULL)
+{
+h = h->next;
+cpt++;
+}
+return (cpt);
+}
+
+
+
+
