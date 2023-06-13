51e153af2f30f75430caa2c6eb5e16367f09d126
mariam-01 <mariam.larhlid2001@gmail.com>
2023-06-13 09:35:08 -0700

    f

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,26 +16,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+int f;
+int l = 0, w = 0;
+if (text_content != NULL)
+{
+l = strlen(text_content);
+}
+if (!filename)
+{
+return (-1);
+}
+f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+w = write(f, text_content, l);
 
-	if (filename == NULL)
-		return (-1);
-
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
-
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
-
-	if (fd == -1 || w == -1)
-		return (-1);
-
-	close(fd);
-
-	return (1);
+if (f == -1 || w == -1)
+{
+return (-1);
 }
 
-
+close(f);
+return (1);
+}
