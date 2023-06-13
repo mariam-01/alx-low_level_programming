1dc448c54e89b0a8182f3cf06bb6be81faec2711
mariam-01 <mariam.larhlid2001@gmail.com>
2023-06-12 19:24:41 -0700

    f

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,27 +12,19 @@
  * @letters: The number of letters to read and print.
  * Return: number of letters read and printed, or 0 if it fails
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int f;
-ssize_t r;
-char buffer[1024];
-if (!filename || !letters)
-{
-return (0);
-}
+char *buf;
+ssize_t f, w, t;
 f = open(filename, O_RDONLY);
-
 if (f == -1)
-{
-return (0);
-}
-r = read(f, buffer, sizeof(buffer));
+	return (0);
+buf = malloc(sizeof(char) * letters);
+t = read(f, buf, letters);
+w = write(STDOUT_FILENO, buf, t);
 
-r = write(STDOUT_FILENO, buffer, r);
-
+free(buf);
 close(f);
-
-return (r);
+return (w);
 }
+
