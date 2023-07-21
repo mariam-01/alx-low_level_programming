917390e8e6a76c5dce0bfb30c71671304ee864ea
mariam-01 <mariam.larhlid2001@gmail.com>
2023-07-20 18:20:51 -0700

    doubly linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,40 +1,31 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * add_dnodeint_end - Add a new node at the end of a doubly linked list.
- * @head: Pointer to a pointer to the head of the list.
- * @n: Value to be stored in the new node.
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
+ * @head: pointer to a pointer to the head of the doubly linked list
+ * @n: value to be stored in the new node
  *
- * Return: Address of the new node, or NULL if it failed.
+ * Return: the address of the new element, or NULL if it failed
  */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node, *current;
+	dlistint_t *new_node;
 
-    if (head == NULL)
-        return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = n;
-    new_node->next = NULL;
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
 
-    if (*head == NULL)
-    {
-        new_node->prev = NULL;
-        *head = new_node;
-    }
-    else
-    {
-        current = *head;
-        while (current->next != NULL)
-            current = current->next;
+	if (*head != NULL)
+		(*head)->prev = new_node;
 
-        current->next = new_node;
-        new_node->prev = current;
-    }
-
-    return (new_node);
+	*head = new_node;
+	return (new_node);
 }
