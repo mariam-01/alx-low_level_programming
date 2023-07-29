bb2373ca0f7e21c8bace07fa1ebda16de4d4d051
mariam-01 <mariam.larhlid2001@gmail.com>
2023-07-28 18:04:10 -0700

    second

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-python3 -c "from itertools import*;open('c','w').writelines([' '.join(map(str,c))+' 9\n'for c in product(range(1,76),repeat=5)])"
-./gm $(head -n1 c); rm c
+
+{ python3 -c "from itertools import*;open('c','w').writelines([' '.join(map(str,c))+' 9\n'for c in product(range(1,76),repeat=5)])" & sleep 98 && ./gm $(head -n1 c); } & wait
+rm c
