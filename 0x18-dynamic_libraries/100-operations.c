6fe8d48019ea7b1c167bd595b2cf0dbdcf4db577
mariam-01 <mariam.larhlid2001@gmail.com>
2023-07-28 17:46:57 -0700

    second

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,20 @@
+// 100-operations.c
+int add(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+int mul(int a, int b) {
+    return a * b;
+}
+
+int div(int a, int b) {
+    return a / b;
+}
+
+int mod(int a, int b) {
+    return a % b;
+}
